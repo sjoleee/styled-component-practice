35869bd3b4cd4fa93a1d3fabb55de72bbf0593e8
sjoleee <cent7425@gmail.com>
2022-07-10 11:50:59 +0900

    styled-components의 기본 사용법을 학습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+
+const BoxTwo = styled.div`
+  background-color: black;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.h2`
+  color: white;
+`;
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <Father>
+      <BoxOne></BoxOne>
+      <BoxTwo>
+        <Text>hello</Text>
+      </BoxTwo>
+    </Father>
+  );
 }
 
 export default App;
