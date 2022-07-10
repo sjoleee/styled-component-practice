48ade60ba221455bf16e2a183317bbaf2377a8e2
sjoleee <cent7425@gmail.com>
2022-07-10 20:28:51 +0900

    dark mode를 구현하기 위한 ThemeProvider 학습

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import styled, { keyframes } from "styled-components";
 import Box from "./components/Box.js";
 
-const Father = styled.div`
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
-    <Father>
+    <Wrapper>
+      <Title>Hello</Title>
       <Box bgColor="blue" text="박스1"></Box>
       <Box bgColor="red" text="박스2"></Box>
-    </Father>
+    </Wrapper>
   );
 }
 
